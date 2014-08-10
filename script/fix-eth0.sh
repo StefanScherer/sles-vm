174be747956421e9d0a89b42a72d1f8a3c6f7095
Stefan Scherer <scherer_stefan@icloud.com>
2014-08-10 16:56:01 +0200

    get eth1 back to eth0

--- script/fix-eth0.sh
+++ script/fix-eth0.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -eux
 
+# http://www.vcritical.com/2010/10/getting-eth0-back-in-a-sles-for-vmware-clone/
+grep -v eth /etc/udev/rules.d/70-persistent-net.rules >/tmp/net.rules
+mv /tmp/net.rules /etc/udev/rules.d/70-persistent-net.rules
+
 # remove NAME= field to allow different network card to appear
 cat <<ETH0 > /etc/sysconfig/network/ifcfg-eth0 
 BOOTPROTO='dhcp'
